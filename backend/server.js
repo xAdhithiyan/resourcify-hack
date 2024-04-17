f01ed936bee32431bdb50789e7267a755b42a99a
xAdhithiyan <adhithiyan.work@gmail.com>
2024-04-17 08:54:30 +0530

    add ev

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ app.use(cors());
 // using middleware -> to understand middle warer
 app.use(express.json());
 
+require('dotenv').config();
+
 // declaring routes
 app.get('/', (req, res) => {
   res.send('hello: Node API');
@@ -63,23 +65,10 @@ app.put('/employee/:id', async (req, res) => {
   }
 });
 
-/*
-// delete a product
-app.delete('/product/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res.status(404).json({ message: 'cannot find any product' });
-    }
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-}); */
-
 mongoose
-  .connect('mongodb+srv://Adhithiyan:quent.work@cluster.44fetee.mongodb.net/')
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}`
+  )
   .then(() => {
     console.log('connected to mongodb');
     app.listen(3000, () => {
